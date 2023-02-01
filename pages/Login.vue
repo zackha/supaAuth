82d9a051e65eb46050d42c44bc287a206460fe11
Zack Hatlen <zckhtln@icloud.com>
2023-02-01 16:22:37 +0300

    Update Login.vue

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,25 +1,9 @@
 <template>
-  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div class="w-full max-w-xs space-y-8">
-      <form @submit.prevent="login" class="mt-8 space-y-6">
-        <div class="-space-y-px rounded-md shadow-sm">
-          <div>
-            <label for="email-address" class="sr-only">Email address</label>
-            <input v-model="email" id="email-address" name="email" type="email" autocomplete="email" class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="test@test.com" />
-          </div>
-          <div>
-            <label for="password" class="sr-only">Password</label>
-            <input v-model="password" id="password" name="password" type="password" autocomplete="current-password" class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Test1234" />
-          </div>
-        </div>
-        <div>
-          <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            Sign in
-          </button>
-        </div>
-      </form>
-    </div>
-  </div>
+  <form @submit.prevent="login">
+    <input type="email" placeholder="email" v-model="email" />
+    <input type="password" placeholder="password" v-model="password" />
+    <button type="submit">Login</button>
+  </form>
 </template>
 
 <script setup lang="ts">
