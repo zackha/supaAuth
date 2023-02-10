8be7a638f8cd401c37581bf3557e001d8f199179
Zack Hatlen <zckhtln@icloud.com>
2023-02-10 18:09:35 +0300

    Update AppHeader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,7 +1,9 @@
 <template>
   <header>
-    <NuxtLink v-if="!user" to="/login"><button>Log in</button></NuxtLink>
-    <button v-else @click="logout">Log out</button>
+    <div style="float:right">
+      <NuxtLink v-if="!user" to="/login"><button>Log in</button></NuxtLink>
+      <button v-else @click="logout">Log out</button>
+    </div>
   </header>
 </template>
 
