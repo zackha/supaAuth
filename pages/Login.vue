c021a9921fa5b6898d778779d75444f68ae55369
Zack Hatlen <zckhtln@icloud.com>
2023-02-15 19:52:33 +0300

    Update Login.vue

--- pages/Login.vue
+++ pages/Login.vue
@@ -1,16 +1,49 @@
 <template>
-  <div class="bg-emptiness text-kuroi-black h-full">
-    <div class="bg-emptiness text-kuroi-black h-screen overflow-y-auto relative">
-      <div class="relative">
-        <form @submit.prevent="login">
-          <input type="text" placeholder="Email address" v-model="email" />
-          <input type="password" placeholder="Password" v-model="password" />
-          <button type="submit">Sign in</button>
-        </form>
+  <div class="buJnuC">
+    <div class="fvfNpo">
+      <div class="uXyMu">
+        <div class="fNAZQD">
+          <div class="dgrFox">
+            <div class="DaoRb">
+              <form @submit.prevent="login">
+                <div class="jGQTZC">
+                  <input type="text" placeholder="Email address" v-model="email" />
+                  <input type="password" placeholder="Password" v-model="password" />
+                </div>
+                <button type="submit">Sign in</button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<style lang="postcss">
+.buJnuC {
+  @apply bg-emptiness text-kuroi-black h-full
+}
+.fvfNpo {
+  @apply bg-emptiness text-kuroi-black h-screen overflow-y-auto relative
+}
+.uXyMu {
+  @apply relative min-h-screen flex justify-center items-center box-border;
+  padding: clamp(92px, 16vh, 200px) 0px
+}
+.fNAZQD {
+  @apply relative flex-grow
+}
+.dgrFox {
+  @apply max-w-[18.75rem] w-full my-0 mx-auto py-0 px-5 box-border 
+}
+.DaoRb {
+  @apply grid gap-8 text-center
+}
+.jGQTZC {
+  @apply grid gap-4 [&:not(:last-child)]:mb-6
+}
+</style>
 
 <script setup lang="ts">
 const email = ref('')
