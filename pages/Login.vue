56d74feed4b7c91ee1f0dc875410145089d79936
Zack Hatlen <zckhtln@icloud.com>
2023-02-17 13:53:15 +0300

    main

--- pages/Login.vue
+++ pages/Login.vue
@@ -85,14 +85,13 @@
   outline: none
 }
 .gZMQdu {
-  --button-border-radius: 5px;
   box-shadow: 0 0 0 var(--button-outline,0px) var(--button-outline-color,#5c89de4d), inset 0 -1px 1px 0px var(--button-inner-shadow,rgba(38 38 44 / 15%)), inset 0 var(--button-shine-y,1px) 0 0 var(--button-shine,rgba(255 255 255 / 25%)), 0 1px 2px 0 var(--button-shadow,rgba(38 38 44 / 25%));
-  @apply text-white bg-[#5c89de] table border-none relative font-semibold text-sm m-0 outline-none select-none whitespace-nowrap transition scale-[var(--button-scale,1)]
-  text-center appearance-none rounded-[var(--button-border-radius)] leading-5 py-[10px] px-6 cursor-pointer tracking-[-0.125px] w-full duration-200
+  @apply text-white bg-[var(--button-background,#5c89de)] table border-none relative font-semibold text-sm m-0 outline-none select-none whitespace-nowrap transition scale-[var(--button-scale,1)]
+  text-center appearance-none rounded-[5px] leading-5 py-[10px] px-6 cursor-pointer tracking-[-0.125px] w-full duration-200
 }
 .gZMQdu:hover {
   --button-shadow: rgba(38 38 44 / 50%);
-  @apply bg-[#3577da]
+  --button-background: #3577da;
 }
 .gZMQdu:active {
   --button-outline: 3px;
