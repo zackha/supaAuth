6f805890ce7332ea80fbe3ec5c65b992bcf817e1
Zack Hatlen <zckhtln@icloud.com>
2023-03-06 12:33:17 +0300

    main

--- pages/Login.vue
+++ pages/Login.vue
@@ -4,37 +4,7 @@
       <div class="uXyMu">
         <div class="fNAZQD">
           <div class="dgrFox">
-            <div class="DaoRb">
-              <h1 class="eSHwvX">Sign in</h1>
-              <form @submit.prevent="login">
-                <div class="jGQTZC">
-                  <label class="iJLvzO">
-                    <div class="fdCSlG">
-                      <input class="cmCuLh" type="text" placeholder="Email address" v-model="email" />
-                    </div>
-                  </label>
-                  <label class="iJLvzO">
-                    <div class="fdCSlG">
-                      <input class="cmCuLh" type="password" placeholder="Password" v-model="password" />
-                    </div>
-                  </label>
-                </div>
-                <div class="jGQTZC">
-                  <button class="gZMQdu" type="submit">
-                    <div class="bjhGPG">Sign in</div>
-                  </button>
-                  <NuxtLink to="/forget-password" class="fTZPOV">Forgot your password?</NuxtLink>
-                </div>
-              </form>
-              <div class="jGQTZC">
-                <p class="dEDhcH">Don’t have a SupaAuth account?</p>
-                <NuxtLink to="/register">
-                  <button class="lcqpaS">
-                    <div class="bjhGPG">Create new account</div>
-                  </button>
-                </NuxtLink>
-              </div>
-            </div>
+            <NuxtPage />
           </div>
         </div>
         <p class="cyDNyc">
@@ -47,22 +17,8 @@
     </div>
   </div>
 </template>
-
 <script setup lang="ts">
-const email = ref('')
-const password = ref('')
-const client = useSupabaseAuthClient()
 const user = useSupabaseUser()
-
-const login = async () => {
-  const { error }  = await client.auth.signInWithPassword({
-    email: email.value,
-    password: password.value
-  })
-  if (error) {
-    return alert('Something went wrong !')
-  }
-}
 
 watchEffect(() => {
   if (user.value) {
---
+++ pages/login/register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="DaoRb">
+    <h1 class="eSHwvX">Create an account</h1>
+  </div>
+</template>
