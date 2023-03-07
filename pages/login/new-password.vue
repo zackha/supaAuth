148a882b14bfafce6fdd94694e42b5ca19b89fad
Zack Hatlen <zckhtln@icloud.com>
2023-03-07 13:23:14 +0300

    Update new-password.vue

--- pages/login/new-password.vue
+++ pages/login/new-password.vue
@@ -3,13 +3,3 @@
     <h1 class="eSHwvX">New password</h1>
   </div>
 </template>
-
-<script setup lang="ts">
-const user = useSupabaseUser()
-
-watchEffect(() => {
-  if (!user.value) {
-    navigateTo('/login')
-  }
-})
-</script>
