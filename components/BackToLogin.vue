5ebdb3d85a3c057549081920abc56c12f2882200
Zack Hatlen <zckhtln@icloud.com>
2023-03-07 10:53:21 +0300

    Update BackToLogin.vue

--- components/BackToLogin.vue
+++ components/BackToLogin.vue
@@ -5,7 +5,7 @@
         <svg viewBox="0 0 20 20" fill="none" class="jXTFjD">
           <path d="M11.6667 6.66663L8.33334 9.99996L11.6667 13.3333" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" style="stroke: var(--icon-color)"></path>
         </svg>
-        Back
+        Login
       </div>
     </NuxtLink>
   </div>
