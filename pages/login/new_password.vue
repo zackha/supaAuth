b1a118872b1b6ed114dcb216aa730268df14d230
Zack Hatlen <zckhtln@icloud.com>
2023-03-08 11:39:10 +0300

    main

--- pages/login/new_password.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="DaoRb">
-    <h1 class="eSHwvX">New password</h1>
-  </div>
-</template>
---
+++ pages/register.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="buJnuC">
+    <div class="fvfNpo">
+      <div class="uXyMu">
+        <BackToLogin />
+        <div class="fNAZQD">
+          <div class="dgrFox">
+            <div class="DaoRb">
+              <h1 class="eSHwvX">Create an account</h1>
+            </div>
+          </div>
+        </div>
+        <p class="cyDNyc">
+          This site is authenticate by supabase.com and the Supabase
+          <a target="_blank" href="https://supabase.com/privacy" rel="noreferrer"> Privacy Policy </a>
+          and <a target="_blank" href="https://supabase.com/terms" rel="noreferrer"> Terms of Service </a>
+          apply.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
