6edfac063b3297eb5710a8c8ba6a71b4d8eaf611
Zack Hatlen <zckhtln@icloud.com>
2023-03-08 16:54:10 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
   <div>
     <AppHeader />
     <div v-if="user">
-      <p>{{ user.user_metadata.first_name }} {{ user.user_metadata.last_name }}</p>
+      <p class="text-xl">Email: {{ user.email }}</p>
+      <p class="text-xl">First Name: {{ user.user_metadata.first_name }}</p>
+      <p class="text-xl">Last Name: {{ user.user_metadata.last_name }}</p>
+      <p class="text-xl">Company: {{ user.user_metadata.company }}</p>
+      <p class="text-xl">last sign in: {{ user.last_sign_in_at }}</p>
     </div>
   </div>
 </template>
