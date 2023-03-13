55e2b249c804e0d20d81e2aa5f3633d60a8079b7
Zack Hatlen <zckhtln@icloud.com>
2023-03-13 19:31:57 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,6 @@ export default defineNuxtConfig({
       viewport: 'width=500, initial-scale=1',
       title: 'supaAuth',
       link: [
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono&display=swap' },
       ],
     }
